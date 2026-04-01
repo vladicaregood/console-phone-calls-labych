19b95be6907cfb346a9d5c0909d4f440bcd74b5a
Vlad icaregood <vladislavs163@gmail.com>
2026-04-01 17:42:03 +0300

    Добавлен проект модульных тестов

---
+++ unit-test-project/unit-test-project.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace unittestproject
+{
+    TEST_CLASS(unittestproject)
+    {
+    public:
+        TEST_METHOD(TestMethod1)
+        {
+            Assert::Fail();
+        }
+    };
+}
